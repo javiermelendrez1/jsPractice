634c535eaf3be3ad6e3a1d1637b18577163a7fe5
javiermelendrez1 <81501034+javiermelendrez1@users.noreply.github.com>
2021-04-29 17:03:45 -0700

    still working on todo app
    figuring out how to check out task

--- jsPractice/todo/app.js
+++ jsPractice/todo/app.js
@@ -6,7 +6,7 @@ const greetingDiv = document.querySelector('.greeting');
 //query for the .container for when we need to append the task containers
 const container = document.querySelector('.container');
 //add an event listener for when the user enters
-
+const ul = document.querySelector('ul');
 textInput.addEventListener('keypress', (e) => {
     //remove the greeting container
     //if the user key press is equal to enter
@@ -16,30 +16,13 @@ textInput.addEventListener('keypress', (e) => {
         const task = textInput.value;
         // console.log(task);
         textInput.value = '';
-        //create task .div
-        // const taskContainer = document.createElement('div');
-        // const span = document.createElement('span');
-        // const checkButton = document.createElement('button');
-        // const taskText = document.createElement('p');
-        // const deleteButton = document.createElement('button');
-        //append all the task items into the span then the span into the task container then the task container onto the regular container
-        // const buttonText = document.createTextNode('O');
-        // const inputText = document.createTextNode(`${task}`);
-        // checkButton.append(buttonText);
-        // taskText.append(inputText);
-        // const deleteButtonText = document.createTextNode('X');
-        // deleteButton.append(deleteButtonText);
-        // span.append(checkButton);
-        // span.append(taskText);
-        // span.append(deleteButton);
-        // taskContainer.append(span);
-        // container.append(taskContainer);
-        const taskContainer = document.createElement('div');
-        taskContainer.className = 'taskContainer';
-        const ul = document.createElement('ul');
+        //const taskContainer = document.createElement('div');
+        //taskContainer.className = 'taskContainer';
+        //const ul = document.createElement('ul');
         const li = document.createElement('li');
         const check = document.createElement('input');
         check.type = 'checkbox';
+        check.id = 'strike';
         const text = document.createElement('p');
         text.append(task);
         const deleteButton = document.createElement('button');
@@ -48,9 +31,19 @@ textInput.addEventListener('keypress', (e) => {
         li.append(check);
         li.append(text);
         li.append(deleteButton);
+        //ul.append(li);
         ul.append(li);
-        taskContainer.append(li);
-        container.append(taskContainer);
-        
+        //container.append(taskContainer);
+        //create the event listener now for the checkbox
+        // const strikethrough =  document.querySelectorAll('#strike');
+
+        // for(let i = 0; i < strikethrough.length; i++){
+        //     strikethrough[i].addEventListener('changed', () => {
+        //         console.log('clicked')
+        //     })
+        // }
+        const line = document.querySelectorAll('#strike').addEventListener('changed', () => {
+            console.log('hello there')
+        })
     }
 })
